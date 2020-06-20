efedf5150e7545c0f3c554644da6bb9abf66de0d
Anton Douginets <anton.douginets@physik.hu-berlin.de>
2020-06-21 01:50:52 +0200

    prep release

---
+++ build.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#########################################################################
+#    Copyright 2020 Anton Douginets
+#    This file is part of solensim.
+#
+#    solensim is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    solensim is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with solensim.  If not, see <https://www.gnu.org/licenses/>.
+#########################################################################
+
+# Release .zip "build" script
+
+# clear previous zip
+rm solensim.zip
+
+# clean up
+rm -rf src/__pycache__
+rm -rf src/mcode/__pycache__
+rm -rf src/pycode/__pycache__
+rm src/octave-workspace
+
+# pack .zip
+mkdir solensim
+cp -r src/* solensim/
+cp LICENSE solensim/
+cp readme.txt solensim/
+zip -r solensim.zip solensim
+
+# clean tmp build dir
+rm -rf solensim
---
+++ readme.txt
@@ -0,0 +1,4 @@
+Solensim v0.1.0
+
+To use, please run solensim.py in an iPython environment of choice,
+for only an iPython interface is so far implemented.
